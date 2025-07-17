ec92aec40ee6417263e53c5cd0707c2c75fc8544
Calidonious <141276027+Calidonious@users.noreply.github.com>
2025-07-17 06:26:54 -0600

    Big update to finalize base config
    fixed all of the problems with current plugins and Lsp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,10 @@ vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { fg = "none" })
 vim.api.nvim_set_hl(0, "NeoTreeFileName", { fg = "#d771fe" })
 vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { fg = "#d771fe" })
 vim.api.nvim_set_hl(0, "NeoTreeSymbolicLinkTarget", { fg = "#d771fe" })
+
+-- Enable persistent undo
+vim.opt.undofile = true
+
+-- Set the directory where undo files will be stored
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undo"
+
