0c1a677251ee98e77ed726f496ad6ee9c073d90d
Calidonious <141276027+Calidonious@users.noreply.github.com>
2025-07-30 16:45:48 -0600

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,6 @@ vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 
 --undo tree keymaps
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+-- neotree keymaps
 vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>')
